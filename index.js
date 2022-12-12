d03d8c6a9f8a2679f869901725d3c53214114abe
thomas <tom.angotti11@gmail.com>
2022-12-12 15:34:36 -0700

    commit tree 6780401f7dff07fb4deee499fb808b4c326e1e85

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+    
+
+function distanceFromHqInBlocks(blocks) {
+    if (blocks >= 42){
+        return blocks - 42;
+    } else { 
+        return 42-blocks;
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start-destination) * 264)
+}
+
+function calculatesFarePrice(start,destination) {
+    let feetTravelled = distanceTravelledInFeet(start, destination);
+    if (feetTravelled <= 400) {
+        return 0
+    } else if(feetTravelled > 400 && feetTravelled <= 2000){
+        return (feetTravelled - 400) * 0.02;
+    } else if (feetTravelled > 2000 && feetTravelled <= 2500){
+        return 25.00;
+    } else if (feetTravelled > 2500) {
+        return 'cannot travel that far';
+    }
+        
+}
+
